03fd9f36e2d51c7f0655595168e75a7db60e5248
Joschka Hüllmann <joschka.huellmann@uni-muenster.de>
2018-05-28 18:59:12 +0200

    initial commit with download, unpack and convert

---
+++ 01_download-2017.sh
@@ -0,0 +1,14 @@
+
+# Download all of 2017
+wget http://data.gharchive.org/2017-01-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-02-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-03-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-04-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-05-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-06-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-07-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-08-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-09-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-10-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-11-{01..30}-{0..23}.json.gz
+# wget http://data.gharchive.org/2017-12-{01..30}-{0..23}.json.gz
---
+++ 02_unpack.sh
@@ -0,0 +1,4 @@
+
+# unpack
+gunzip *.gz
+
